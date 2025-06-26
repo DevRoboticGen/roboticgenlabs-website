139e47593b4effd3d74063ff8990c61d6b1faf42
Sanjula Gathsara <sanjulagathsara@gmail.com>
2025-06-27 02:10:34 +0530

    This commit is the initial commit

---
+++ company-website/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-indigo-500 to-purple-600 text-white flex justify-center items-center text-4xl">
+      ✅ Tailwind is working!
+    </div>
+  );
+}
